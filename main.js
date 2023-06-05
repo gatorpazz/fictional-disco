dabcb60306dc261b87d5303545804e9c9bfa96d9
Michael Pascuzzi <gatorpazz@gmail.com>
2023-06-05 16:09:42 -0600

    rendering orgs

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 const BASE_URL = 'https://api.github.com'
+const listEl = document.querySelector('.list-container');
 
 async function getGithubOrgs() {
   try {
     const response = await fetch(`${BASE_URL}/organizations`);
     const data = await response.json();
-    console.log(data);
+    return data;
   } catch (err) {
     console.log(err);
   }
 }
 
-getGithubOrgs();
+function renderGithubOrgs(orgs) {
+  const template = orgs.map(org => `
+    <li class="list-item">${org.login}</li>
+  `);
+  listEl.innerHTML = template.join('');
+}
+
+const init = async () => {
+  const orgs = await getGithubOrgs();
+  renderGithubOrgs(orgs);
+}
+
+init();
